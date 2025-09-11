57d4deacda282de0c5b7739e7d516a3cb9518578
GuilhermeCintraUchoa <cintratop@gmail.com>
2025-09-11 13:28:20 -0300

    AAAAAAAAAAAAAAAAAAA

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("greetings boots")
+from stats import get_num_words
+
+def get_book_text(file_path):
+    with open(file_path) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    file_string = get_book_text("books/frankenstein.txt")
+    
+    word_count = get_num_words(file_string)
+
+    print(f"{word_count} words found in the document")
+
+
+main()
---
+++ stats.py
@@ -0,0 +1,11 @@
+def get_num_words(text_book):
+    text_split = text_book.split()
+    num_words = len(text_split)
+    return num_words
+
+def get_num_characters(text_book):
+    lowered_text = text_book.lower()
+
+
+
+
